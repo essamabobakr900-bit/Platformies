6dadb47f36f217041adabfe71274ca58b6b2afad
essamabobakr900-bit <essamabobakr900@gmail.com>
2026-04-05 11:59:26 +0200

    i finished designing the tile map of the game and started working on the enemies and collectable items.

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,6 +1,8 @@
 extends CharacterBody2D
 
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@onready var jump_sound: AudioStreamPlayer2D = $jump_sound
+
 
 
 const SPEED = 300.0
@@ -18,9 +20,11 @@ func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 		animated_sprite_2d.animation = "jump"
+		
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		jump_sound.play()
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.

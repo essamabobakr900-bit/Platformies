801a7512c5fa03392f78d3609b600265d311a7ec
essamabobakr900-bit <essamabobakr900@gmail.com>
2026-04-06 00:43:42 +0200

    i finished designing the enemies and now i am adding it collision shapes

--- scripts/snail.gd
+++ scripts/snail.gd
@@ -1,5 +1,7 @@
 extends Area2D
 
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+signal player_died
 const SPEED = 100.0
 var direction = -1.0
 
@@ -11,3 +13,18 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	position.x += direction * SPEED * delta
+
+
+func _on_timer_timeout() -> void:
+	direction *= -1
+	animated_sprite_2d.flip_h = !animated_sprite_2d.flip_h
+	
+
+
+
+	
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.name == "player":
+		emit_signal("player_died", body)
+		

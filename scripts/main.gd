de31433de27d28abf7ee8a4f686482404e30f64e
essamabobakr900-bit <essamabobakr900@gmail.com>
2026-04-06 06:03:09 +0200

    i succesfully finished the first level of the game  and now designing the second one.

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,6 +1,6 @@
 extends Node2D
 @onready var score_label: Label = $CanvasLayer/ScorePanel/Score_Label
-
+var level: int = 1
 var score: int = 0
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -11,6 +11,11 @@ func _ready() -> void:
 func _process(_delta: float) -> void:
 	pass
 func _setup_level() -> void:
+	var exit = $level_root.get_node_or_null("Exit")
+	if exit:
+		exit.body_entered.connect(_on_exit_body_entered)
+		
+		
 	var apples = $level_root.get_node_or_null("Apples")
 	if apples:
 		for enemy in apples.get_children():
@@ -20,6 +25,14 @@ func _setup_level() -> void:
 	if enemies:
 		for enemy in enemies.get_children():
 			enemy.player_died.connect(_on_player_died)
+
+func _on_exit_body_entered(body: Node2D) -> void:
+	if body.name == "Player":
+		level += 1
+		print(body)
+		print(level)
+		body.can_move = false
+		
 
 
 func _on_player_died(body):
--- scripts/player.gd
+++ scripts/player.gd
@@ -9,6 +9,7 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -850.0
 var alive = true
+var can_move = true
 
 func _physics_process(delta: float) -> void:
 	#add animation
@@ -24,25 +25,27 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 		animated_sprite_2d.animation = "jump"
 		
-	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-		jump_sound.play()
+		
+	if can_move:
+		# Handle jump.
+		if Input.is_action_just_pressed("jump") and is_on_floor():
+			velocity.y = JUMP_VELOCITY
+			jump_sound.play()
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("left", "right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		# Get the input direction and handle the movement/deceleration.
+		# As good practice, you should replace UI actions with custom gameplay actions.
+		var direction := Input.get_axis("left", "right")
+		if direction:
+			velocity.x = direction * SPEED
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
 
-	move_and_slide()
-	
-	if direction == 1.0:
-		animated_sprite_2d.flip_h = false
-	elif direction == -1.0:
-		animated_sprite_2d.flip_h = true
+		move_and_slide()
+		
+		if direction == 1.0:
+			animated_sprite_2d.flip_h = false
+		elif direction == -1.0:
+			animated_sprite_2d.flip_h = true
 
 
 func die() -> void:

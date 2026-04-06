57b858b41c796cb71b16ceb84659e350013806ed
essamabobakr900-bit <essamabobakr900@gmail.com>
2026-04-06 02:58:53 +0200

    i finished the collectable apples section and i am now working on the panel that will desplay the points or score

--- scripts/apple.gd
+++ scripts/apple.gd
@@ -1,15 +1,25 @@
 extends Area2D
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@onready var collected_apple_sound: AudioStreamPlayer2D = $Collected_Apple_sound
+@onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
 
+signal collected
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
 
 
 func _on_body_entered(body: Node2D) -> void:
-	pass # Replace with function body.
+	if body.name == "Player":
+		animated_sprite_2d.animation = "collected"
+		collected_apple_sound.play()
+		collected.emit()
+		call_deferred("_disable_collision")
+		
+func _disable_collision() -> void:
+		collision_shape_2d.disabled = true
+
+	
+
+
+func _on_animated_sprite_2d_animation_looped() -> void:
+	if animated_sprite_2d.animation == "collected":
+		queue_free()
--- scripts/main.gd
+++ scripts/main.gd
@@ -1,15 +1,20 @@
 extends Node2D
 
-
+var score: int = 0
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	_setup_level()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	pass
 func _setup_level() -> void:
+	var apples = $level_root.get_node_or_null("Apples")
+	if apples:
+		for enemy in apples.get_children():
+			enemy.collected.connect(increase_score)
+			
 	var enemies = $level_root.get_node_or_null("Enemies")
 	if enemies:
 		for enemy in enemies.get_children():
@@ -20,3 +25,8 @@ func _on_player_died(body):
 	body.die()
 	print("Player killed")
 	
+	
+func increase_score() -> void:
+	score += 1
+	print(score)
+		
